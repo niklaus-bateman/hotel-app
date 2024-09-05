73126e5c98ede5b601d51d9b62627434ec5edd5d
Fardeen Khan <khan.fardeen.dhn@gmail.com>
2024-09-05 21:29:34 +0530

    supabse issue fixed

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,7 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { getCabins } from "../services/apiCabins";
+import Spinner from '../ui/Spinner'
 
 function Cabins() {
+  const {data:cabins , isLoading} = useQuery({
+    queryKey: ['cabins'],
+    queryFn: getCabins
+  })
+  console.log(cabins)
+  if(isLoading) return <Spinner/>
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
