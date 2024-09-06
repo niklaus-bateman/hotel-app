51cff8daebabc268efe8a91e3126cd55d4acc6fa
Fardeen Khan <khan.fardeen.dhn@gmail.com>
2024-09-07 00:33:13 +0530

    toasts-configured

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -3,19 +3,20 @@ import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import { getCabins } from "../services/apiCabins";
 import Spinner from '../ui/Spinner'
+import CabinTable from "../features/cabins/CabinTable";
 
 function Cabins() {
-  const {data:cabins , isLoading} = useQuery({
-    queryKey: ['cabins'],
-    queryFn: getCabins
-  })
-  console.log(cabins)
-  if(isLoading) return <Spinner/>
+
   return (
+    <>
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-    </Row>
+      <p>Filter/Sort</p>
+      </Row>
+      <Row>
+      <CabinTable/>
+      </Row>
+    </>
   );
 }
 
